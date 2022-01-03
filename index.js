8d0bba2e6aac153d4eab9589706e62145b91cb1e
Justin Reid <jayarghargh@gmail.com>
2022-01-04 08:06:27 +1000

    add exports to index

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ function setConfig() {
     global.DIST_3D = "dist_3d";
 }
 
-module.exports = {
-    setConfig
-};
+// module.exports = {
+//     setConfig
+// };
+exports.BearingDistanceFromCoords = require('./lib/BearingDistanceFromCoords');
+exports.BearingFromDeltas = require('./lib/BearingFromDeltas');
+exports.CommonCalculations = require('./lib/CommonCalculations');
+exports.ConvertDegreesToRadians = require('./lib/ConvertDegreesToRadians');
+exports.ConvertRadiansToDegrees = require('./lib/ConvertRadiansToDegrees');
+exports.ConvertToDecimalDegrees = require('./lib/ConvertToDecimalDegrees');
+exports.ConvertToDegreesMinutesSeconds = require('./lib/ConvertToDegMinSec');
+exports.CoordsFromBearingDistance = require('./lib/CoordsFromBearingDistance');
+exports.DistanceSpeedCalcs = require('./lib/DistanceFromSpeedTime');
+exports.FormatDegreesMinutesSeconds = require('./lib/FormatDegMinSec');
+exports.SurveyPoint = require('./lib/SurveyPoint');
+module.exports = {setConfig};
